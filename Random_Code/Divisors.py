e72d84bfd97741f12d02176da3100d69d941b15d
Arif Uz Zaman Badhon <md.arifuz.zaman@g.bracu.ac.bd>
2021-04-15 12:34:56 +0600

    Update Divisors.py

--- Random_Code/Divisors.py
+++ Random_Code/Divisors.py
@@ -1,3 +1,8 @@
 #Create a program that asks the user for a number and then 
 # prints out a list of all the divisors of that number
 
+def get_divisors(n):
+    for i in range(1, int(n / 2) + 1):
+        if n % i == 0:
+            yield i
+    yield n
